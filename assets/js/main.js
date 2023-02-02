fa16fb790017277248471c5c4c6f39814f79518f
alamgirweb11 <alamgirweb11@gmail.com>
2023-02-03 02:10:29 +0600

    value section added with custom accordion

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,31 @@ var swiperPopular = new Swiper(".popular__container", {
 });
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item')
 
+accordionItems.forEach(item => {
+    const accordionHeader = item.querySelector('.value__accordion-header')
+    accordionHeader.addEventListener('click', () => {
+      const openAccordionItem = document.querySelector('.accordion-open')
+      toggleAccordion(item)
+
+      if(openAccordionItem && openAccordionItem !== item){
+         toggleAccordion(openAccordionItem)
+      }
+    })
+})
+
+const toggleAccordion = (item) => {
+   const accordionContent = item.querySelector('.value__accordion-content');
+   
+   if(item.classList.contains('accordion-open')){
+      accordionContent.removeAttribute('style')
+      item.classList.remove('accordion-open')
+   } else {
+    accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+   }
+}
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
